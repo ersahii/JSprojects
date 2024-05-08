ff0ab9e18e71b7dbbbe39422016c7cceefe42259
mirmehraj <mehraj95he@gmail.com>
2024-05-08 12:54:28 +0530

    Added initial files and generated random number

---
+++ script.js
@@ -0,0 +1,6 @@
+prompt("Enter name of first person")
+prompt("Enter name of second person")
+var ranNum = Math.random();
+ranNum *= 99;
+ranNum = Math.floor(ranNum) + 1;
+alert(`Your love score is ${ranNum}%`);
