f0e64035b46f27061b5d57e625daef7905454105
mirmehraj <mehraj95he@gmail.com>
2024-05-08 14:27:46 +0530

    added conditional to main

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ prompt("Enter name of second person")
 var ranNum = Math.random();
 ranNum *= 99;
 ranNum = Math.floor(ranNum) + 1;
-alert(`Your love score is ${ranNum}%`);
+if(ranNum === 100){
+    alert(`Your love score is ${ranNum}%. You are perfect match for each other`);
+}
+else if ( ranNum > 60 && ranNum<= 80 ){
+    alert(`Your love score is ${ranNum}%, You are going good in your relation`);
+}
+else{
+    alert(`Your love score is ${ranNum}%`);
+}
